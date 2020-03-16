9c8cfd7cec4dbe514dfdafcdd5c5a406082e79fc
Дом <printks@yandex.ru>
2020-03-16 20:22:56 +0300

    implement HW_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int size = 0; //количество резюме в storage
+    private int size = 0;                                   //количество резюме в storage
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
     }
 
     void save(Resume r) {
-        Resume save = r;
+        storage[size] = r;
     }
 
     Resume get(String uuid) {
@@ -26,15 +26,14 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int i;
-        for (i = 0; i < size; i++) {                //поиск удаляемого элемента
+        for (i = 0; i < size; i++) {                        //поиск удаляемого элемента
             if (storage[i].uuid == uuid) {
-                break;
+                for (int j = i; j < size - 1; j++) {        //сдвиг последующих элементов
+                    storage[j] = storage[j + 1];
+                }
+                size--;                                     //уменьшение количества резюме
             }
         }
-        for (int j = i; j < size - 1; j++) {        //сдвиг последующих элементов
-            storage[j] = storage[j + 1];
-        }
-        size--;                                     //уменьшение количества резюме
     }
 
     /**
