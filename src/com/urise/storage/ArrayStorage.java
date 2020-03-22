fcdf6af55d42e6bbf9a1b9e92e490f95a742efa6
Дом <printks@yandex.ru>
2020-03-22 15:32:47 +0300

    Add static class members

--- src/com/urise/storage/ArrayStorage.java
+++ src/com/urise/storage/ArrayStorage.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    public static final int STORAGE_LIMIT = 10_000;
+    private static final int STORAGE_LIMIT = 10_000;
     private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
@@ -27,7 +27,7 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (size > storage.length) {
+        if (size > STORAGE_LIMIT) {
             System.out.println("Переполнение размера storage!");
         } else if (getIndex(resume.getUuid()) != -1) {
             System.out.println("Резюме " + resume + " есть в storage!");
