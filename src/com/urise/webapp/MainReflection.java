ed51665fb51c64f0e9c95b1824421aba7887fb72
Дом <printks@yandex.ru>
2020-03-29 22:35:50 +0300

    add invoke method toString (Reflection)

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,9 +3,11 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -13,5 +15,10 @@ public class MainReflection {
         System.out.println(field.get(resume));
         field.set(resume, "new_uuid");
         System.out.println(resume);
+
+//        Class clazz = Class.forName(Resume.class.getName());
+//        Method toString = clazz.getDeclaredMethod("toString");
+        Method toString = resume.getClass().getDeclaredMethod("toString");
+        System.out.print("invoke method " + toString.getName() + " : " + toString.invoke(resume));
     }
 }
