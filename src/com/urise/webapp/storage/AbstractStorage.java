412eb6d395a0305149ebc421f6d1f2c4e151e19c
Дом <printks@yandex.ru>
2020-04-02 19:33:05 +0300

    HW_5 takeaway AbstractStorage

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -1,41 +1,52 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
 
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
     public void update(Resume resume) {
-
+        int index = getIndex(resume.getUuid());
+        if (index < 0) {
+            throw new NotExistStorageException(resume.getUuid());
+        } else {
+            implementUpdate(resume, resume.getUuid());
+        }
     }
 
-    @Override
     public void save(Resume resume) {
-
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            throw new ExistStorageException(resume.getUuid());
+        } else {
+            implementSave(resume, resume.getUuid());
+        }
     }
 
-    @Override
     public Resume get(String uuid) {
-        return null;
+        int index = getIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        return implementGet(uuid);
     }
 
-    @Override
     public void delete(String uuid) {
-
+        int index = getIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        } else {
+            implementDelete(uuid);
+        }
     }
+    protected abstract void implementUpdate(Resume resume, String uuid);
 
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
-    }
+    protected abstract void implementSave(Resume resume, String uuid);
 
-    @Override
-    public int size() {
-        return 0;
-    }
+    protected abstract Resume implementGet(String uuid);
+
+    protected abstract void implementDelete(String uuid);
+
+    protected abstract int getIndex(String uuid);
 }
