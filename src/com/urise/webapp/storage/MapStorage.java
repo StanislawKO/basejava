bc176e3d223a3680e6846f11517219416e5f342b
Дом <printks@yandex.ru>
2020-04-04 13:43:48 +0300

    HW_5 add ListStorageTest

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -2,54 +2,44 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ListStorage extends AbstractStorage {
-    private List<Resume> list = new ArrayList<>();
-
+public class MapStorage extends AbstractStorage {
     @Override
     protected void doUpdate(Resume resume, int index) {
-        list.set(index, resume);
+
     }
 
     @Override
     protected void doSave(Resume resume, int index) {
-        list.add(resume);
+
     }
 
     @Override
     protected Resume doGet(int index) {
-        return list.get(index);
+        return null;
     }
 
     @Override
     protected void doDelete(int index) {
-        list.remove(index);
+
     }
 
     @Override
     protected int getIndex(String uuid) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getUuid().equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
+        return 0;
     }
 
     @Override
     public void clear() {
-        list.clear();
+
     }
 
     @Override
     public Resume[] getAll() {
-        return list.toArray(new Resume[list.size()]);
+        return new Resume[0];
     }
 
     @Override
     public int size() {
-        return list.size();
+        return 0;
     }
 }
