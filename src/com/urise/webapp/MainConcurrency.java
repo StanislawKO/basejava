498707563029c4461b843592bc136d95c4224fa9
Дом <printks@yandex.ru>
2020-04-21 20:55:22 +0300

    HW_11 HW-deadlock

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -23,7 +23,7 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
-        
+
         for (int i = 0; i < THREADS_NUMBER; i++) {
             Thread thread = new Thread(new Runnable() {
                 @Override
@@ -47,12 +47,35 @@ public class MainConcurrency {
         });
 //        Thread.sleep(500);
         System.out.println(count);
+
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
+    }
+
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("Waiting " + lock1);
+            synchronized (lock1) {
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2);
+                synchronized (lock2) {
+                    System.out.println(("Holding " + lock2));
+                }
+            }
+        }).start();
     }
 
     private static synchronized void inc() {
 //        double a = Math.sin(13);
 //        synchronized (LOCK) {
-            count++;
+        count++;
 //        }
     }
 }
